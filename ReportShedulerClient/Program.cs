bc7242516a257772687f526a90c7845774e4596f
ssvvs <ssv_vs@mail.ru>
2018-11-18 14:06:55 +0700

    Added new project

--- ReportShedulerClient/Program.cs
+++ ReportShedulerClient/Program.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ReportShedulerClient
+﻿namespace ReportShedulerClient
 {
     class Program
     {
